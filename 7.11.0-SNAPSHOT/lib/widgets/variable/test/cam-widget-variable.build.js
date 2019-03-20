9375eefacb91720dfe120face34c0ebce3e50bdf
camunda-jenkins <ci@camunda.com>
2019-03-20 13:06:10 +0100

    "gh-pages update"

--- 7.11.0-SNAPSHOT/lib/widgets/variable/test/cam-widget-variable.build.js
+++ 7.11.0-SNAPSHOT/lib/widgets/variable/test/cam-widget-variable.build.js
@@ -185,7 +185,7 @@ varUtils.validate = function($scope) {
 
 module.exports = varUtils;
 
-},{"camunda-bpm-sdk-js/lib/forms/type-util":6,"camunda-bpm-sdk-js/vendor/angular":9}],2:[function(require,module,exports){
+},{"camunda-bpm-sdk-js/lib/forms/type-util":8,"camunda-bpm-sdk-js/vendor/angular":9}],2:[function(require,module,exports){
 /*
  * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
  * under one or more contributor license agreements. See the NOTICE file
@@ -237,7 +237,7 @@ module.exports = [function() {
   };
 }];
 
-},{"camunda-bpm-sdk-js/lib/forms/type-util":6}],3:[function(require,module,exports){
+},{"camunda-bpm-sdk-js/lib/forms/type-util":8}],3:[function(require,module,exports){
 /*
  * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
  * under one or more contributor license agreements. See the NOTICE file
@@ -4312,130 +4312,6 @@ return 'pascalprecht.translate';
 }));
 
 },{}],6:[function(require,module,exports){
-/*
- * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
- * under one or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information regarding copyright
- * ownership. Camunda licenses this file to you under the Apache License,
- * Version 2.0; you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-'use strict';
-
-var INTEGER_PATTERN = /^-?[\d]+$/;
-
-var FLOAT_PATTERN = /^(0|(-?(((0|[1-9]\d*)\.\d+)|([1-9]\d*))))([eE][-+]?[0-9]+)?$/;
-
-var BOOLEAN_PATTERN = /^(true|false)$/;
-
-var DATE_PATTERN = /^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}(|\.[0-9]{0,4})$/;
-
-var xmlParser = require('../../vendor/fast-xml-parser');
-
-var isValidXML = function(value) {
-  return value ? xmlParser.validate(value) : false;
-};
-
-var isValidJSON = function(value) {
-  try {
-    JSON.parse(value);
-    return true;
-  } catch (e) {
-    return false;
-  }
-};
-
-var isType = function(value, type) {
-  switch(type) {
-  case 'Integer':
-  case 'Long':
-  case 'Short':
-    return INTEGER_PATTERN.test(value);
-  case 'Float':
-  case 'Double':
-    return FLOAT_PATTERN.test(value);
-  case 'Boolean':
-    return BOOLEAN_PATTERN.test(value);
-  case 'Date':
-    return DATE_PATTERN.test(dateToString(value));
-  case 'Xml':
-    return isValidXML(value);
-  case 'Json':
-    return isValidJSON(value);
-  }
-};
-
-var convertToType = function(value, type) {
-
-  if(typeof value === 'string') {
-    value = value.trim();
-  }
-
-  if(type === 'String' || type === 'Bytes' || type === 'File') {
-    return value;
-  } else if (isType(value, type)) {
-    switch(type) {
-    case 'Integer':
-    case 'Long':
-    case 'Short':
-      return parseInt(value, 10);
-    case 'Float':
-    case 'Double':
-      return parseFloat(value);
-    case 'Boolean':
-      return 'true' === value;
-    case 'Date':
-      return dateToString(value);
-    }
-  } else {
-    throw new Error('Value \''+value+'\' is not of type '+type);
-  }
-};
-
-/**
- * This reformates the date into a ISO8601 conform string which will mirror the selected date in local format.
- * TODO: Remove this when it is fixed by angularjs
- *
- * @see https://app.camunda.com/jira/browse/CAM-4746
- *
- */
-var pad = function(number) {
-  return ( number < 10 ) ?  '0' + number : number;
-};
-
-var dateToString = function(date) {
-  if( typeof date === 'object' && typeof date.getFullYear === 'function' ) {
-    var year    = date.getFullYear(),
-        month   = pad( date.getMonth() + 1 ),
-        day     = pad( date.getDate() ),
-        hour    = pad( date.getHours() ),
-        min = pad( date.getMinutes() ),
-        sec = pad( date.getSeconds() );
-
-    return year + '-' + month + '-' + day + 'T' + hour + ':' + min + ':' + sec;
-
-  } else {
-    return date;
-
-  }
-};
-
-module.exports = {
-  convertToType : convertToType,
-  isType : isType,
-  dateToString : dateToString
-};
-
-},{"../../vendor/fast-xml-parser":10}],7:[function(require,module,exports){
 /**
  * @license AngularJS v1.7.8
  * (c) 2010-2018 Google, Inc. http://angularjs.org
@@ -40871,11 +40747,135 @@ $provide.value("$locale", {
 })(window);
 
 !window.angular.$$csp().noInlineStyle && window.angular.element(document.head).prepend('<style type="text/css">@charset "UTF-8";[ng\\:cloak],[ng-cloak],[data-ng-cloak],[x-ng-cloak],.ng-cloak,.x-ng-cloak,.ng-hide:not(.ng-hide-animate){display:none !important;}ng\\:form{display:block;}.ng-animate-shim{visibility:hidden;}.ng-anchor{position:absolute;}</style>');
-},{}],8:[function(require,module,exports){
+},{}],7:[function(require,module,exports){
 require('./angular');
 module.exports = angular;
 
-},{"./angular":7}],9:[function(require,module,exports){
+},{"./angular":6}],8:[function(require,module,exports){
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+var INTEGER_PATTERN = /^-?[\d]+$/;
+
+var FLOAT_PATTERN = /^(0|(-?(((0|[1-9]\d*)\.\d+)|([1-9]\d*))))([eE][-+]?[0-9]+)?$/;
+
+var BOOLEAN_PATTERN = /^(true|false)$/;
+
+var DATE_PATTERN = /^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}(|\.[0-9]{0,4})$/;
+
+var xmlParser = require('../../vendor/fast-xml-parser');
+
+var isValidXML = function(value) {
+  return value ? xmlParser.validate(value) : false;
+};
+
+var isValidJSON = function(value) {
+  try {
+    JSON.parse(value);
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
+var isType = function(value, type) {
+  switch(type) {
+  case 'Integer':
+  case 'Long':
+  case 'Short':
+    return INTEGER_PATTERN.test(value);
+  case 'Float':
+  case 'Double':
+    return FLOAT_PATTERN.test(value);
+  case 'Boolean':
+    return BOOLEAN_PATTERN.test(value);
+  case 'Date':
+    return DATE_PATTERN.test(dateToString(value));
+  case 'Xml':
+    return isValidXML(value);
+  case 'Json':
+    return isValidJSON(value);
+  }
+};
+
+var convertToType = function(value, type) {
+
+  if(typeof value === 'string') {
+    value = value.trim();
+  }
+
+  if(type === 'String' || type === 'Bytes' || type === 'File') {
+    return value;
+  } else if (isType(value, type)) {
+    switch(type) {
+    case 'Integer':
+    case 'Long':
+    case 'Short':
+      return parseInt(value, 10);
+    case 'Float':
+    case 'Double':
+      return parseFloat(value);
+    case 'Boolean':
+      return 'true' === value;
+    case 'Date':
+      return dateToString(value);
+    }
+  } else {
+    throw new Error('Value \''+value+'\' is not of type '+type);
+  }
+};
+
+/**
+ * This reformates the date into a ISO8601 conform string which will mirror the selected date in local format.
+ * TODO: Remove this when it is fixed by angularjs
+ *
+ * @see https://app.camunda.com/jira/browse/CAM-4746
+ *
+ */
+var pad = function(number) {
+  return ( number < 10 ) ?  '0' + number : number;
+};
+
+var dateToString = function(date) {
+  if( typeof date === 'object' && typeof date.getFullYear === 'function' ) {
+    var year    = date.getFullYear(),
+        month   = pad( date.getMonth() + 1 ),
+        day     = pad( date.getDate() ),
+        hour    = pad( date.getHours() ),
+        min = pad( date.getMinutes() ),
+        sec = pad( date.getSeconds() );
+
+    return year + '-' + month + '-' + day + 'T' + hour + ':' + min + ':' + sec;
+
+  } else {
+    return date;
+
+  }
+};
+
+module.exports = {
+  convertToType : convertToType,
+  isType : isType,
+  dateToString : dateToString
+};
+
+},{"../../vendor/fast-xml-parser":10}],9:[function(require,module,exports){
 /*
  * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
  * under one or more contributor license agreements. See the NOTICE file
@@ -40897,7 +40897,7 @@ module.exports = angular;
 
 module.exports = require('angular');
 
-},{"angular":8}],10:[function(require,module,exports){
+},{"angular":7}],10:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, '__esModule', { value: true });
