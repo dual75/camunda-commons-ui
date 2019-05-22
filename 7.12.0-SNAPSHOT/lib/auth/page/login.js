0069c6b0b1bebe01a35668c607ae2450f405e76c
camunda-jenkins <ci@camunda.com>
2019-05-22 14:22:45 +0200

    "gh-pages update"

---
+++ 7.12.0-SNAPSHOT/lib/auth/page/login.js
@@ -0,0 +1,82 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+var fs = require('fs');
+
+var template = fs.readFileSync(__dirname + '/login.html', 'utf8');
+
+var $ = require('jquery');
+
+var Controller = [
+  '$scope',
+  '$rootScope',
+  'AuthenticationService',
+  'Notifications',
+  '$location',
+  '$translate',
+  function(
+    $scope,
+    $rootScope,
+    AuthenticationService,
+    Notifications,
+    $location,
+    $translate
+  ) {
+    if ($rootScope.authentication) {
+      return $location.path('/');
+    }
+
+    $rootScope.showBreadcrumbs = false;
+
+    // ensure focus on username input
+    var autofocusField = $('form[name="signinForm"] [autofocus]')[0];
+    if (autofocusField) {
+      autofocusField.focus();
+    }
+
+    $scope.login = function() {
+      return AuthenticationService
+        .login($scope.username, $scope.password)
+        .then(function() {
+          Notifications.clearAll();
+        })
+        .catch(function(error) {
+          delete $scope.username;
+          delete $scope.password;
+
+          Notifications.addError({
+            status: $translate.instant('PAGE_LOGIN_FAILED'),
+            message: (error.data && error.data.message) || $translate.instant('PAGE_LOGIN_ERROR_MSG'),
+            scope: $scope,
+            exclusive: true
+          });
+        });
+    };
+  }];
+
+module.exports = [
+  '$routeProvider',
+  function(
+    $routeProvider
+  ) {
+    $routeProvider.when('/login', {
+      template: template,
+      controller: Controller
+    });
+  }];
